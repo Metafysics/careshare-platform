207d487a22a80593d13b335760d0ba4d3c52ccd7
Metafysics <steven@metafysics.be>
2025-07-11 14:35:49 +0200

    Create SuperDashboard.jsx

---
+++ frontend/src/pages/SuperDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SuperDashboard = () => {
+  return (
+    <div className="max-w-7xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">Super Dashboard</h1>
+      {/* Voeg hier overzichts- en beheerfuncties toe zoals boekingen, betalingen, statistieken, etc. */}
+      <p>Hier komt het overzicht van alle belangrijke beheermodules.</p>
+    </div>
+  );
+};
+
+export default SuperDashboard;
